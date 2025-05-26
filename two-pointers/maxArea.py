50ac1f91d4670751996ff39f16a19bbc6ba34324
ayogenthiran <yanojan96@gmail.com>
2025-05-26 09:20:29 -0400

    Add Container With Most Water solution with O(n) time complexity

--- two-pointers/maxArea.py
+++ two-pointers/maxArea.py
@@ -1,21 +1,27 @@
 class Solution:
     def maxArea(self, heights):
         res = 0
-        l, r = 0, len(heights)-1
+        l, r = 0, len(heights) - 1
 
-        while l<r:
-            area = min(heights[l], heights[r]) * (r-l) # Finding the area using minimum height
+        while l < r:
+            area = (r-l) * min(heights[l], heights[r])
             res = max(res, area)
 
-            if heights[l] <= heights[r]:
+            if heights[l] < heights[r]:
                 l += 1
+            elif heights[l] > heights[r]:
+                r -= 1
             else:
+                l += 1
                 r -= 1
         return res
     
-
 # Test
 sol = Solution()
-height = [1,8,6,2,5,4,8,3,7]
-x = sol.maxArea(height)
+heights = [1,8,6,2,5,4,8,3,7]
+x = sol.maxArea(heights)
 print(x)
+
+
+
+
