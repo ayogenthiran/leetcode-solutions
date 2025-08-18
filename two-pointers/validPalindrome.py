3c767183cea2a8501c14f8c1959b6e23b884c3b2
ayogenthiran <yanojan96@gmail.com>
2025-08-18 13:25:54 -0400

    Implemented custom isalnum() with ord() checks for palindrome validation

--- two-pointers/validPalindrome.py
+++ two-pointers/validPalindrome.py
@@ -1,28 +1,29 @@
 class Solution:
-    def is_palindrome(self, s):
-        l, r = 0, len(s) - 1
-        
-        while l < r:
-            while l < r and not self.is_alpha_num(s[l]):
-                l += 1
-            while r > l and not self.is_alpha_num(s[r]):
-                r -= 1
+    def isPalindrome(self,s):
 
-            if s[l].lower() != s[r].lower():
+        left,right = 0, len(s) - 1  
+        while left < right:
+
+            while left < right and not self.isalnum(s[left]):
+                left += 1
+            while left > right and not self.isalnum(s[right]):
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
             
-            l, r = l + 1, r - 1 
-        return True
-        
-    def is_alpha_num(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
-        
+            # Move both pointers inward
+            left += 1
+            right -= 1
+        return True  
+
+    def isalnum(self,c):
+        return ( ord('A') <= ord(c) <= ord('Z') or
+                 ord('a') <= ord(c) <= ord('z') or
+                 ord('0') <= ord(c) <= ord('9') )    
 # Test
 sol = Solution()
 s = "tab a cat"
-x = sol.is_palindrome(s)
+x = sol.isPalindrome(s)
 print(x)
 
 # Time Complexity: O(n)
