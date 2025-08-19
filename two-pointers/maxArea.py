ba4aeb0150cbedc666c3ae4050318be7d02cf08a
ayogenthiran <yanojan96@gmail.com>
2025-08-19 15:53:07 -0400

    Adding answer to largest container

--- two-pointers/maxArea.py
+++ two-pointers/maxArea.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxArea(self, heights):
-        res = 0
         l, r = 0, len(heights) - 1
+        max_area = 0
 
         while l < r:
-            area = (r-l) * min(heights[l], heights[r])
-            res = max(res, area)
+            area = min(heights[l], heights[r]) * (r - l)
+            max_area = max(area, max_area)
 
             if heights[l] < heights[r]:
                 l += 1
@@ -14,7 +14,7 @@ class Solution:
             else:
                 l += 1
                 r -= 1
-        return res
+        return max_area
     
 # Test
 sol = Solution()
