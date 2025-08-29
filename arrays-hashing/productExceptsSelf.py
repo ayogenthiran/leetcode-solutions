dabd6b3864ca1e6313040a3fe7316c37fd2c211d
ayogenthiran <yanojan96@gmail.com>
2025-08-28 21:57:37 -0400

    Update solution for productExceptSelf

---
+++ arrays-hashing/productExceptsSelf.py
@@ -0,0 +1,22 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
+
+
+# Test
+x = [1,2,3,4]
+sol = Solution()
+output = sol.productExceptSelf(x)
+print("output:", output)
